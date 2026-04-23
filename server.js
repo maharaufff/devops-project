fa4f0956b2e6bec6a5e22f73e43abb821b21a215
maharaufff <FA23-BCS-078@cuilahore.edu.pk>
2026-04-23 06:00:38 +0500

    Removed node_modules and added gitignore

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.static(__dirname));
+
+
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from Backend" });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
